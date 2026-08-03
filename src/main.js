daa9b59a8cdc9a81db846bc94a365ce6fe6cb200
TarasBilyi <taras.bilyi@gmail.com>
2026-08-03 21:44:04 +0300

    some fix

--- src/main.js
+++ src/main.js
@@ -2,3 +2,12 @@ import './js/burgerMenu';
 import './js/headerScroll';
 import './js/featureSwipper';
 import './js/gallerySwipper';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  duration: 800,
+  once: true,
+  offset: 0,
+  startEvent: 'DOMContentLoaded',
+});
